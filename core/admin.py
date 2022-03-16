559e99395d46aeb7206dbfb1ac3040358254a2b2
AmirHmn <amir.hosein6979@gmail.com>
2022-03-16 20:17:44 +0330

    add Question model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question, Answer
+
 
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+
 # Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.text
+
+
+
