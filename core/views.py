c9ff48f4f6bfff65bf768d31fee6be864bc5e92c
AmirHmn <amir.hosein6979@gmail.com>
2022-03-18 00:08:03 +0330

    create view for questions

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class QuestionView(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
