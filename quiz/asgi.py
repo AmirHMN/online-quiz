8515feedee6147cde88692d2f894401b18b8723f
AmirHmn <amir.hosein6979@gmail.com>
2022-04-12 21:33:45 +0430

    setup loading environment variables from .env

--- quiz/asgi.py
+++ quiz/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import dotenv
 from django.core.asgi import get_asgi_application
 
+dotenv.read_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz.settings.dev')
 
 application = get_asgi_application()
