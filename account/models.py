16a0427f53c14672cf0582d02a77a9b0f07cba60
AmirHmn <amir.hosein6979@gmail.com>
2022-04-12 11:21:41 +0430

    add custom validator for national code

--- account/models.py
+++ account/models.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 from .managers import UserManager
+
+national_code_regex = RegexValidator(regex=r"^([0-9]){10}$")
 
 
 class User(AbstractUser):
     username_validator = None
     username = None
     email = None
-    national_code = models.CharField(max_length=10, unique=True, verbose_name='کد ملی')
+    national_code = models.CharField(max_length=10, unique=True, verbose_name='کد ملی',
+                                     validators=[national_code_regex])
     phone_number = models.CharField(max_length=12, verbose_name='شماره تلفن')
     USERNAME_FIELD = 'national_code'
     REQUIRED_FIELDS = ['first_name', 'last_name']
